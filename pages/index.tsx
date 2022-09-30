cb0b5b1f210326899b6764757021b41d10aa1cd7
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-09-30 22:35:05 +0300

    Test out prisma in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
-import type { NextPage } from "next";
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+import type { GetServerSideProps, NextPage } from "next";
 
-const Home: NextPage = () => {
-  return <div>homepage</div>;
+type HomeProps = {
+  userCount: Prisma.GetUserAggregateType<{ _count: true }>;
+};
+
+const Home: NextPage<HomeProps> = ({ userCount }) => {
+  return <pre>{JSON.stringify(userCount, null, 2)}</pre>;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const userCount = await prisma.user.aggregate({
+    _count: true,
+  });
+
+  return {
+    props: { userCount },
+  };
 };
 
 export default Home;
