805a4fd973e2560a4d27e3d19d73e6378cbec3d9
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-09-30 23:40:11 +0300

    Create login page with markup

---
+++ pages/auth/login.tsx
@@ -0,0 +1,70 @@
+import {
+  Button,
+  createStyles,
+  NumberInput,
+  PasswordInput,
+  Stack,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { NextPage } from "next";
+
+const INPUT_SIZE = "lg";
+const CITIZEN_ID_LENGTH = 11;
+
+const useStyles = createStyles({
+  root: {
+    paddingTop: 128,
+    maxWidth: 420,
+    margin: "auto",
+  },
+});
+
+const Login: NextPage = () => {
+  const { classes } = useStyles();
+  const form = useForm({
+    initialValues: {
+      citizenId: "",
+      password: "",
+    },
+    validate: {
+      citizenId: (value) =>
+        String(value).length !== CITIZEN_ID_LENGTH &&
+        "TC Kimlik Numarası 11 haneli olmalıdır",
+      password: (value) =>
+        String(value).length < 6 && "Şifre en az 6 karakter olmalıdır",
+    },
+  });
+
+  const handleSubmit = form.onSubmit((values) => {
+    console.log(values);
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Stack className={classes.root}>
+        <Title>Deneme Grafik'e Giriş</Title>
+        <NumberInput
+          hideControls
+          label="TC Kimlik Numarası"
+          placeholder="12345678901"
+          maxLength={CITIZEN_ID_LENGTH}
+          formatter={(value) => value?.replace(/\D/g, "")}
+          size={INPUT_SIZE}
+          {...form.getInputProps("citizenId")}
+        />
+        <PasswordInput
+          label="Parola"
+          placeholder="sifre123"
+          size={INPUT_SIZE}
+          {...form.getInputProps("password")}
+        />
+        <Button size={INPUT_SIZE} type="submit">
+          Giriş yap
+        </Button>
+      </Stack>
+    </form>
+  );
+};
+
+export default Login;
