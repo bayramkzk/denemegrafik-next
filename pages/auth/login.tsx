35a9f44d1851958efb58205707e0de1ecddafe19
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-10-03 23:50:12 +0300

    Integrate new next-auth logic to login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,4 @@
 import { CITIZEN_ID_LENGTH, MINIMUM_PASSWORD_LENGTH } from "@/constants/index";
-import { useAuth } from "@/hooks/use-auth";
 import {
   Button,
   createStyles,
@@ -11,8 +10,8 @@ import {
 import { useForm } from "@mantine/form";
 import { showNotification } from "@mantine/notifications";
 import { NextPage } from "next";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const INPUT_SIZE = "lg";
 
@@ -27,7 +26,6 @@ const useStyles = createStyles({
 
 const Login: NextPage = () => {
   const router = useRouter();
-  const { login, user } = useAuth();
 
   const { classes } = useStyles();
   const form = useForm({
@@ -45,26 +43,26 @@ const Login: NextPage = () => {
     },
   });
 
-  useEffect(() => {
-    if (user) {
-      showNotification({
-        title: `Tekrar hoşgeldin ${user.student.name}`,
-        message: "Giriş başarılı, yönlendiriliyorsunuz...",
-      });
-      router.push("/");
-    }
-  }, [user]);
-
   const handleSubmit = form.onSubmit(async (values) => {
-    const res = await login({ ...values, citizenId: String(values.citizenId) });
-    if (!res.data.success && !(res.data.error instanceof Array)) {
+    const res = await signIn("credentials", {
+      redirect: false,
+      ...values,
+    });
+    if (!res) return;
+
+    if (res.error) {
       showNotification({
         title: "Giriş başarısız",
-        message:
-          res.data.error.message === "invalid_credentials"
-            ? "TC Kimlik Numarası veya şifre hatalı"
-            : "Bilinmeyen bir hata oluştu",
+        message: res.error,
       });
+    }
+
+    if (res.ok) {
+      showNotification({
+        title: "Giriş başarılı",
+        message: "Yönlendiriliyorsunuz...",
+      });
+      router.push("/");
     }
   });
 
