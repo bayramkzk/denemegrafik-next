c92f1266bf8ea70d4509ea0380cb0f3154e66ea6
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-10-09 02:12:27 +0300

    Create auth response types for login and register

--- types/auth.ts
+++ types/auth.ts
@@ -1,6 +1,39 @@
 import { Admin, Class, School, Student, User } from "@prisma/client";
+import { ZodIssue } from "zod";
 
 export type SessionUser = Omit<User, "hash"> & {
   student: (Student & { class: Class & { school: School } }) | null;
   admin: (Admin & { school: School }) | null;
 };
+
+export type AuthValidationErrorResponse = {
+  error: ZodIssue[];
+  success: false;
+};
+
+export type AuthCustomErrorResponse = {
+  error: {
+    code: string;
+    message: string;
+  };
+  success: false;
+};
+
+export type AuthErrorResponse =
+  | AuthValidationErrorResponse
+  | AuthCustomErrorResponse;
+
+export type LoginSuccessResponse = {
+  user: SessionUser;
+  token: string;
+  success: true;
+};
+
+export type LoginResponse = AuthErrorResponse | LoginSuccessResponse;
+
+export type RegisterSuccessResponse = {
+  user: SessionUser;
+  success: true;
+};
+
+export type RegisterResponse = AuthErrorResponse | RegisterSuccessResponse;
