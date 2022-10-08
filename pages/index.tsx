f7663e86ac8720ad14814605da13effac83f6865
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-10-08 11:14:46 +0300

    Add profile table and ssr result chart to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,55 @@
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
+import ProfileTable from "@/components/ProfileTable";
 import SessionGuard from "@/components/SessionGuard";
 import { prisma } from "@/lib/prisma";
-import { Container, Title } from "@mantine/core";
-import { Test, TestResult } from "@prisma/client";
+import { TestResultWithTest } from "@/types/tests";
+import { Alert, Container, Stack, Text, Title } from "@mantine/core";
+import { useElementSize } from "@mantine/hooks";
+import { IconInfoCircle } from "@tabler/icons";
 import type { GetServerSideProps, NextPage } from "next";
 import { unstable_getServerSession } from "next-auth";
+import dynamic from "next/dynamic";
 import { authOptions } from "./api/auth/[...nextauth]";
 
+const ResultChart = dynamic(() => import("@/components/ResultChart"), {
+  ssr: false,
+});
+
 export type HomeProps = {
-  results: (TestResult & { test: Test })[];
+  results: TestResultWithTest[];
 };
 
 const Home: NextPage<HomeProps> = ({ results }) => {
+  const { ref, width } = useElementSize();
+
   return (
-    <Layout navbar={<Navbar />}>
-      <Container>
-        <SessionGuard>
-          {(session) => (
-            <div>
-              <Title mt="xl">Home</Title>
-              <p>Hi {session.user.student.name}!</p>
-              <p>Here are your results:</p>
-              <ul>
-                {results.map((result) => (
-                  <li key={result.id}>
-                    {result.test.name}: {result.score}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </SessionGuard>
-      </Container>
-    </Layout>
+    <SessionGuard>
+      {(session) => (
+        <Layout navbar={!session.user.student && <Navbar />}>
+          <Container ref={ref}>
+            <Stack py="lg" spacing={32}>
+              <Title>Deneme Grafik</Title>
+
+              <Alert variant="filled" icon={<IconInfoCircle />}>
+                Merhaba {session.user.student.name}, bu websitesindeki veriler
+                sadece senin için saklanmaktadır.
+              </Alert>
+
+              <ProfileTable />
+
+              <Text>
+                Aşağıdaki grafiği inceleyerek deneme sonuçlarının zaman içinde
+                nasıl değiştiğini görebilirsin ve kendi sonuçlarını ortalama ile
+                karşılaştırabilirsin.
+              </Text>
+
+              <ResultChart results={results} width={width} />
+            </Stack>
+          </Container>
+        </Layout>
+      )}
+    </SessionGuard>
   );
 };
 
