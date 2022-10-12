ad923ee737cbdf0092ca1e3c8f64cc850dc79de8
highberg <48856944+highberg@users.noreply.github.com>
2022-10-13 00:01:06 +0300

    Create name and school utils for user

--- utils/user.ts
+++ utils/user.ts
@@ -1,9 +1,21 @@
+import { SessionUser } from "@/types/auth";
 import { Class } from "@prisma/client";
 
 export const stringifyClass = (cls: Class) => `${cls.grade} / ${cls.branch}`;
 
-export const getFirstName = (name: string) =>
+export const parseFirstName = (name: string) =>
   name.split(" ").slice(0, -1).join(" ");
 
-export const getLastName = (name: string) =>
+export const parseLastName = (name: string) =>
   name.split(" ").slice(-1).join(" ");
+
+export const parseIntoNames = (name: string) => ({
+  firstName: parseFirstName(name),
+  lastName: parseLastName(name),
+});
+
+export const getName = (user: SessionUser) =>
+  user.student ? user.student.name : user.admin!.name;
+
+export const getSchool = (user: SessionUser) =>
+  user.student ? user.student.class.school : user.admin!.school;
