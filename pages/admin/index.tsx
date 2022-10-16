112c8718b406b4e5bf34d1ca25fdf262301c40ac
highberg <48856944+highberg@users.noreply.github.com>
2022-10-17 02:33:54 +0300

    Create empty admin page with only title

---
+++ pages/admin/index.tsx
@@ -0,0 +1,18 @@
+import Layout from "@/components/Layout";
+import SessionGuard from "@/components/SessionGuard";
+import { Title } from "@mantine/core";
+import { NextPage } from "next";
+
+const Admin: NextPage = (props) => {
+  return (
+    <SessionGuard allowedRoles={["ADMIN", "SUPERADMIN"]}>
+      {({ user }) => (
+        <Layout>
+          <Title>Admin Panel</Title>
+        </Layout>
+      )}
+    </SessionGuard>
+  );
+};
+
+export default Admin;
