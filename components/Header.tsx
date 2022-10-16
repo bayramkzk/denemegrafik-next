3e769711b344b0a45729cec92118d381a711da57
highberg <48856944+highberg@users.noreply.github.com>
2022-10-17 02:35:17 +0300

    Add links to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,22 @@
+import { getLinkDataBasedOnRole } from "@/utils/links";
 import {
   Burger,
   createStyles,
+  Group,
   Header as MantineHeader,
   MediaQuery,
+  Paper,
+  Transition,
 } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import Link from "next/link";
-import { Dispatch, SetStateAction } from "react";
 import { HEADER_HEIGHT } from "../constants";
 import AppLogo from "./AppLogo";
+import HeaderLink from "./HeaderLink";
+import SessionGuard from "./SessionGuard";
 import UserMenu from "./UserMenu";
+
+const HAMBURGER_BREAKPOINT = "sm";
 
 const useStyles = createStyles((theme) => ({
   burger: {
@@ -17,45 +25,100 @@ const useStyles = createStyles((theme) => ({
     backgroundColor:
       theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
   },
+
   header: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     height: "100%",
   },
+
+  links: {
+    [theme.fn.smallerThan(HAMBURGER_BREAKPOINT)]: {
+      display: "none",
+    },
+  },
+
+  dropdown: {
+    position: "absolute",
+    top: HEADER_HEIGHT,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopWidth: 0,
+    overflow: "hidden",
+
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
 }));
 
-export type HeaderProps = {
-  burgerState: [boolean, Dispatch<SetStateAction<boolean>>];
-  burgerEnabled: boolean;
-};
+export type HeaderProps = {};
 
-const Header: React.FC<HeaderProps> = ({ burgerState }) => {
-  const [burgerOpened, setBurgerOpened] = burgerState;
+const Header: React.FC<HeaderProps> = (props) => {
   const { classes } = useStyles();
+  const [burgerOpened, burgerHandler] = useDisclosure(false);
 
   return (
-    <MantineHeader height={HEADER_HEIGHT} p="sm" className={classes.header}>
-      {burgerOpened && (
-        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-          <Burger
-            className={classes.burger}
-            opened={burgerOpened}
-            onClick={() => setBurgerOpened((o) => !o)}
-            size="sm"
-            mr="xl"
-          />
-        </MediaQuery>
-      )}
+    <SessionGuard>
+      {({ user }) => {
+        const linkData = getLinkDataBasedOnRole(user.role);
+        const nestedLinks = linkData.map((link) => <HeaderLink link={link} />);
+        const flatLinks = linkData.map((link) => (
+          <HeaderLink link={link} flattenMenu closeMenu={burgerHandler.close} />
+        ));
 
-      <Link href="/" passHref>
-        <a>
-          <AppLogo size={20} />
-        </a>
-      </Link>
+        return (
+          <MantineHeader
+            height={HEADER_HEIGHT}
+            p="sm"
+            className={classes.header}
+          >
+            <MediaQuery
+              largerThan={HAMBURGER_BREAKPOINT}
+              styles={{ display: "none" }}
+            >
+              <Burger
+                className={classes.burger}
+                opened={burgerOpened}
+                onClick={burgerHandler.toggle}
+                size="sm"
+                mr="xl"
+              />
+            </MediaQuery>
 
-      <UserMenu />
-    </MantineHeader>
+            <Group>
+              <Link href="/" passHref>
+                <a>
+                  <AppLogo size={20} />
+                </a>
+              </Link>
+
+              <Group spacing="xl" className={classes.links}>
+                {nestedLinks}
+              </Group>
+            </Group>
+
+            <UserMenu />
+
+            <Transition
+              transition="pop-top-right"
+              duration={200}
+              mounted={burgerOpened}
+            >
+              {(styles) => (
+                <Paper className={classes.dropdown} withBorder style={styles}>
+                  {flatLinks}
+                </Paper>
+              )}
+            </Transition>
+          </MantineHeader>
+        );
+      }}
+    </SessionGuard>
   );
 };
 
