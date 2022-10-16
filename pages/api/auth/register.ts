b62297818ef16661a4973210f2ede5cdbd922f5f
highberg <48856944+highberg@users.noreply.github.com>
2022-10-16 20:10:39 +0300

    Refactor register api according to new db schema

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -6,36 +6,42 @@ import {
 import { PASSWORD_SALT_OR_ROUNDS } from "@/constants/index";
 import { prisma } from "@/lib/prisma";
 import { registerSchema } from "@/schemas/register";
-import { RegisterResponse, StudentUser } from "@/types/auth";
+import { RegisterResponse } from "@/types/auth";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import bcrypt from "bcrypt";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler<RegisterResponse> = async (req, res) => {
   const body = registerSchema.safeParse(req.body);
-  if (!body.success)
+  if (!body.success) {
     return res.status(400).json({ error: body.error.issues, success: false });
+  }
 
-  const student = await prisma.student.findUnique({
+  const student = await prisma.profile.findUnique({
     where: { citizenId: body.data.citizenId },
   });
-  if (!student) return res.status(400).json(NO_STUDENT_FOUND);
+  if (!student) {
+    return res.status(400).json(NO_STUDENT_FOUND);
+  }
 
   const hash = await bcrypt.hash(body.data.password, PASSWORD_SALT_OR_ROUNDS);
+
   try {
     const user = await prisma.user.create({
-      data: { citizenId: body.data.citizenId, hash },
+      data: {
+        profile: { connect: { citizenId: body.data.citizenId } },
+        hash,
+      },
       include: {
-        student: { include: { class: { include: { school: true } } } },
-        admin: { include: { school: true } },
+        profile: {
+          include: { group: { include: { organization: true } } },
+        },
       },
     });
-    if (user.role !== "STUDENT" || !user.student) {
-      throw Error("Registered user is not student");
-    }
-    const { hash: _, ...userWithoutHash } = user;
+
+    const { hash: _, ...userWithoutHash } = { ...user, profile: user.profile! };
     return res.status(200).json({
-      user: userWithoutHash as StudentUser,
+      user: userWithoutHash,
       success: true,
     });
   } catch (error) {
