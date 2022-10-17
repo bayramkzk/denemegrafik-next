95e348dbf57b268ec7a6a8e5cc72fbc875dac197
highberg <48856944+highberg@users.noreply.github.com>
2022-10-18 01:51:59 +0300

    Query data from api in DatabaseModelPage

--- pages/admin/database/[model]/index.tsx
+++ pages/admin/database/[model]/index.tsx
@@ -5,30 +5,70 @@ import {
   DatabaseModel,
   DatabaseModelPluralDisplayNames,
 } from "@/constants/models";
-import { Title } from "@mantine/core";
+import { Routes } from "@/constants/routes";
+import { Stack, Text, Title } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
+import { showNotification } from "@mantine/notifications";
+import { IconDatabaseOff } from "@tabler/icons";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { DataTable } from "mantine-datatable";
 import { GetServerSideProps, NextPage } from "next";
+import { useEffect } from "react";
 
-export type DatabaseProps = {
+export type DatabaseModelPageServerSideProps = {
   model: DatabaseModel;
 };
 
-const DatabaseModelPage: NextPage<DatabaseProps> = ({ model }) => {
-  const title = DatabaseModelPluralDisplayNames[model] ?? "Veriler";
+export type DatabaseModelPageProps = DatabaseModelPageServerSideProps & {};
+
+const DatabaseModelPage: NextPage<DatabaseModelPageProps> = ({ model }) => {
+  const title = DatabaseModelPluralDisplayNames[model];
   const { height } = useViewportSize();
+  const minHeight = height / 2;
+  const {
+    data: res,
+    isLoading,
+    error,
+  } = useQuery([model], () => axios.get(`${Routes.databaseApi}/${model}`));
+
+  useEffect(() => {
+    if (!res?.data?.success) {
+      console.error(error);
+      showNotification({
+        title: "Veri çekme başarısız",
+        message: error
+          ? String(error)
+          : res?.data
+          ? JSON.stringify(res?.data)
+          : "Bilinmeyen bir hata oluştu",
+        color: "red",
+      });
+    }
+  }, [res]);
 
   return (
     <SessionGuard>
       {() => (
         <Layout>
-          <Title>{title}</Title>
+          <Stack>
+            <Title>{title}</Title>
 
-          <DataTable
-            my={50}
-            columns={modelToColumnMap[model]}
-            minHeight={height / 2}
-          />
+            <DataTable
+              my={50}
+              columns={modelToColumnMap[model]}
+              records={res?.data}
+              fetching={isLoading}
+              loaderBackgroundBlur={1}
+              minHeight={minHeight}
+              emptyState={
+                <Stack align="center">
+                  <IconDatabaseOff size={40} />
+                  <Text>{title} için veri bulunamadı</Text>
+                </Stack>
+              }
+            />
+          </Stack>
         </Layout>
       )}
     </SessionGuard>
@@ -37,9 +77,9 @@ const DatabaseModelPage: NextPage<DatabaseProps> = ({ model }) => {
 
 export default DatabaseModelPage;
 
-export const getServerSideProps: GetServerSideProps<DatabaseProps> = async (
-  context
-) => {
+export const getServerSideProps: GetServerSideProps<
+  DatabaseModelPageServerSideProps
+> = async (context) => {
   const model = (context.query.model as string | undefined) ?? null;
 
   if (!model || !Object.keys(DatabaseModelPluralDisplayNames).includes(model)) {
