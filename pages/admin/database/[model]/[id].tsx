cc4cc897fb60d08f4de57c9d5e1c068671ba8fb9
highberg <48856944+highberg@users.noreply.github.com>
2022-10-18 01:56:23 +0300

    Create DatabaseModelIdPage subroute

---
+++ pages/admin/database/[model]/[id].tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+interface DatabaseModelIdPageProps {
+  id: string;
+}
+
+const DatabaseModelIdPage: NextPage<DatabaseModelIdPageProps> = () => {
+  return <div>Database Model Id Page</div>;
+};
+
+export default DatabaseModelIdPage;
