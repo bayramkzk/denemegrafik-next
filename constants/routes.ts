1193d4d8c76bf06f65d4009d19775819e44afb4e
highberg <48856944+highberg@users.noreply.github.com>
2022-10-18 01:56:05 +0300

    Refactor routes and add database routes

--- constants/routes.ts
+++ constants/routes.ts
@@ -6,20 +6,5 @@ export const Routes = Object.freeze({
 
   loginApi: "/api/auth/login",
   registerApi: "/api/auth/register",
+  databaseApi: "/api/database",
 });
-
-type RouteKey = keyof typeof Routes;
-const ProtectedRouteKeys: readonly RouteKey[] = Object.freeze(["home"]);
-
-export const ProtectedRoutes = Object.freeze(
-  Object.fromEntries(ProtectedRouteKeys.map((key) => [key, Routes[key]]))
-);
-
-export const UnprotectedRoutes = Object.freeze(
-  Object.fromEntries(
-    Object.keys(Routes)
-      .map((key) => key as RouteKey)
-      .filter((key) => !ProtectedRouteKeys.includes(key))
-      .map((key) => [key, Routes[key]])
-  )
-);
