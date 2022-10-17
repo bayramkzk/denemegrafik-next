8463b91d76fcfdcb801a4ac3fa00a0cf330bbe6d
highberg <48856944+highberg@users.noreply.github.com>
2022-10-17 23:46:40 +0300

    Remove menu options from HeaderLink

--- components/HeaderLink.tsx
+++ components/HeaderLink.tsx
@@ -1,6 +1,5 @@
 import { HeaderLinkData } from "@/types/header";
-import { Center, createStyles, Menu } from "@mantine/core";
-import { IconChevronDown } from "@tabler/icons";
+import { createStyles } from "@mantine/core";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
@@ -9,7 +8,7 @@ const useStyles = createStyles((theme) => ({
   link: {
     display: "block",
     lineHeight: 1,
-    padding: "8px 12px",
+    padding: "12px",
     borderRadius: theme.radius.sm,
     textDecoration: "none",
     color:
@@ -24,11 +23,6 @@ const useStyles = createStyles((theme) => ({
         theme.colorScheme === "dark"
           ? theme.colors.dark[6]
           : theme.colors.gray[0],
-    },
-
-    [theme.fn.smallerThan("sm")]: {
-      borderRadius: 0,
-      padding: theme.spacing.md,
     },
   },
 
@@ -50,79 +44,21 @@ const useStyles = createStyles((theme) => ({
 
 export interface HeaderLinkProps {
   link: HeaderLinkData;
-  closeMenu?: () => void;
-  flattenMenu?: boolean;
+  closeMenu: () => void;
 }
 
-const HeaderLink: React.FC<HeaderLinkProps> = ({
-  link,
-  flattenMenu,
-  closeMenu,
-}) => {
+const HeaderLink: React.FC<HeaderLinkProps> = ({ link, closeMenu }) => {
   const router = useRouter();
   const { classes, cx } = useStyles();
 
-  if (link.hasMenu) {
-    if (flattenMenu) {
-      return (
-        <>
-          {link.links.map((subLink) => (
-            <Link href={subLink.href} key={subLink.href}>
-              <a
-                onClick={(e) => {
-                  e.preventDefault();
-                  router.push(subLink.href);
-                  if (closeMenu) closeMenu();
-                }}
-                className={cx(
-                  classes.link,
-                  router.asPath === subLink.href && classes.linkActive
-                )}
-              >
-                {subLink.label}
-              </a>
-            </Link>
-          ))}
-        </>
-      );
-    }
-
-    const menuItems = link.links.map((item) => (
-      <Menu.Item key={item.href} onClick={() => router.push(item.href)}>
-        {item.label}
-      </Menu.Item>
-    ));
-
-    return (
-      <Menu key={link.label} trigger="hover" exitTransitionDuration={0}>
-        <Menu.Target>
-          <a
-            href={link.href}
-            onClick={(e) => {
-              e.preventDefault();
-              router.push(link.href);
-            }}
-            className={cx(classes.link, {
-              [classes.linkActive]: router.pathname === link.href,
-            })}
-          >
-            <Center>
-              <span className={classes.linkLabel}>{link.label}</span>
-              <IconChevronDown size={12} stroke={1.5} />
-            </Center>
-          </a>
-        </Menu.Target>
-        <Menu.Dropdown>{menuItems}</Menu.Dropdown>
-      </Menu>
-    );
-  }
-
   return (
-    <Link href={link.href}>
+    <Link href={link.href} key={link.href}>
       <a
-        key={link.label}
-        className={classes.link}
-        onClick={(event) => event.preventDefault()}
+        onClick={() => closeMenu()}
+        className={cx(
+          classes.link,
+          router.asPath === link.href && classes.linkActive
+        )}
       >
         {link.label}
       </a>
