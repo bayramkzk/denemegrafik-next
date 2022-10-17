8246f1c780ed4d13de7f2d8e4b4e4e1405e679bc
highberg <48856944+highberg@users.noreply.github.com>
2022-10-17 03:22:18 +0300

    Add data grid to database page

--- pages/admin/database.tsx
+++ pages/admin/database.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
 import SessionGuard from "@/components/SessionGuard";
+import { modelToColumnMap } from "@/constants/columns";
 import {
   DatabaseModel,
   DatabaseModelPluralDisplayNames,
 } from "@/constants/models";
 import { Title } from "@mantine/core";
+import { DataGrid } from "mantine-data-grid";
 import { GetServerSideProps, NextPage } from "next";
 
 export type DatabaseProps = {
@@ -13,11 +15,23 @@ export type DatabaseProps = {
 
 const Database: NextPage<DatabaseProps> = ({ model }) => {
   const title = DatabaseModelPluralDisplayNames[model] ?? "Veriler";
+
   return (
     <SessionGuard>
       {() => (
         <Layout>
           <Title>{title}</Title>
+
+          {model ? (
+            <DataGrid
+              my={50}
+              data={[{}]}
+              withGlobalFilter
+              columns={modelToColumnMap[model]}
+            />
+          ) : (
+            <p>Model seçilmedi.</p>
+          )}
         </Layout>
       )}
     </SessionGuard>
