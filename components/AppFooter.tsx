04709b52b7f9f13363f4575b1db04c321cc7250f
highberg <48856944+highberg@users.noreply.github.com>
2022-10-18 00:49:29 +0300

    Remove margin top from AppFooter

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -5,7 +5,6 @@ import { CONTAINER_SIZE } from "../constants";
 
 const useStyles = createStyles((theme) => ({
   footer: {
-    marginTop: 120,
     borderTop: `1px solid ${
       theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
     }`,
