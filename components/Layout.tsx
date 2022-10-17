53aa4214166c299bbb7c57e33e8d9fbebb37a360
highberg <48856944+highberg@users.noreply.github.com>
2022-10-18 00:18:33 +0300

    Remove aside and configure footer in Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,4 @@
-import {
-  AppShell,
-  Aside,
-  Container,
-  createStyles,
-  Footer,
-  MediaQuery,
-} from "@mantine/core";
+import { AppShell, Container, createStyles } from "@mantine/core";
 import { CONTAINER_SIZE } from "../constants";
 import { AppFooter } from "./AppFooter";
 import Header from "./Header";
@@ -23,41 +16,20 @@ const useStyles = createStyles((theme) => ({
 
 export type LayoutProps = {
   children: React.ReactNode;
-  aside?: React.ReactNode;
-  footer?: React.ReactNode;
 };
 
-const Layout: React.FC<LayoutProps> = ({ children, aside, footer }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   const { classes } = useStyles();
 
   return (
     <AppShell
       className={classes.root}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      aside={
-        aside ? (
-          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
-            <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-              {aside}
-            </Aside>
-          </MediaQuery>
-        ) : undefined
-      }
-      footer={
-        footer ? (
-          <Footer height={60} p="md">
-            {footer}
-          </Footer>
-        ) : undefined
-      }
+      footer={<AppFooter />}
       header={<Header />}
     >
       <Container size={CONTAINER_SIZE} py="xl">
         {children}
       </Container>
-
-      <AppFooter />
     </AppShell>
   );
 };
